ac10cc5e4234bff6df9ea3dd31061199e218546c
Syamsul MJ <syamsulmj94@gmail.com>
2026-07-02 17:16:29 +0800

    add offline page if no internet connections and put the static page in the static files assets to be compiled

--- lib/prep_mech_web.ex
+++ lib/prep_mech_web.ex
@@ -17,7 +17,8 @@ defmodule PrepMechWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths,
+    do: ~w(assets fonts images favicon.ico robots.txt manifest.json sw.js offline.html)
 
   def router do
     quote do
